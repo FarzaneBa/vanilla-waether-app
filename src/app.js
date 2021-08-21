8bef7887cd2ab2444048bb95a86876be75a3f7ef
farzaneb75 <77022867+farzaneb75@users.noreply.github.com>
2021-08-21 20:13:13 +0430

    Update app.js

--- src/app.js
+++ src/app.js
@@ -31,7 +31,6 @@ function getForcast(coordinates) {
   let lon = coordinates.lon;
   let apiKey = `53647c12f67effb0b2c13aa8c1aa18af`;
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(dispalyForcast);
 }
 // display weather description
